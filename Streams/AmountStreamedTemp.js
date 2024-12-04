989ecd94c6f9d703ab9ab636f28e79b2d4f1b89b
ianmunge0 <ianmunge0@protonmail.com>
2024-12-04 12:47:56 +0300

    improved UX of streamed amount

--- Streams/AmountStreamedTemp.js
+++ Streams/AmountStreamedTemp.js
@@ -22,17 +22,39 @@ function useInterval(callback, delay) {
 export default function AmountStreamedTemp({rate, updatedattimestamp, streameduntilupdatedat, inlistview}) {
 
   const[difftime, setDifftime] = useState((new Date().getTime()/1000) - updatedattimestamp)
+  const[dp, setDp] = useState(0);
 
-  useInterval(() => setDifftime((new Date().getTime()/1000) - updatedattimestamp), 250)
+  useInterval(() => setDifftime((new Date().getTime()/1000) - updatedattimestamp), 225)
 
   let numstreamed = ((Number(rate) * difftime) / 1000000000000000000) + (Number(streameduntilupdatedat) / 1000000000000000000)
+  let numstreamedstr = String(numstreamed)
+  
+  if (String(numstreamed).includes('.') == true) {
+    if (String(numstreamed).split('.')[1].length > 18) {
+      numstreamedstr =
+        String(numstreamed).split('.')[0] +
+        '.' +
+        String(numstreamed).split('.')[1].substring(0, 18);
+    }
+    
+    if (String(numstreamed).split('.')[1].length < dp){
+      numstreamedstr = 
+        String(numstreamed).split('.')[0] +
+        '.' +
+        String(numstreamed).split('.')[1].padEnd(dp, '0')
+    }
+    
+    if (String(numstreamed).split('.')[1].length > dp && String(numstreamed).split('.')[1].length <= 18) {
+      setDp(String(numstreamed).split('.')[1].length);
+    }
+  }
 
   return(
     <>
     {inlistview == true ?
       <Text style={{color: '#15D828', fontSize: 15}}>{numstreamed.toString().length > 5 ? numstreamed.toString().substring(0, 5)+"..." : numstreamed}</Text>
     :
-      <Text style={{color: "#15D828" , fontFamily: 'Rubik', marginLeft: 12, fontSize: 15}}>{numstreamed} cUSDx</Text>}
+      <Text style={{color: "#15D828" , fontFamily: 'Rubik', marginLeft: 12, fontSize: 15}}>{numstreamedstr} cUSDx</Text>}
     </>
   )
 };
